cdb42464a3ed7b4a936379c4c1cb98216b9b153e
Cody Bontecou <bontecouc@gmail.com>
2020-04-22 10:15:46 -0500

    moved js logic into their own files

---
+++ listen.js
@@ -0,0 +1,54 @@
+window.addEventListener("DOMContentLoaded", () => {
+    const button = document.getElementById("button");
+    const result = document.getElementById("result");
+    const main = document.getElementsByTagName("main")[0];
+    let listening = false;
+    const SpeechRecognition =
+        window.SpeechRecognition || window.webkitSpeechRecognition;
+    if (typeof SpeechRecognition !== "undefined") {
+        const recognition = new SpeechRecognition();
+        // recognition.lang = "es-MX"
+
+        const stop = () => {
+            main.classList.remove("speaking");
+            recognition.stop();
+            button.textContent = "Start listening";
+            listening = false;
+        };
+
+        const start = () => {
+            main.classList.add("speaking");
+            recognition.start();
+            button.textContent = "Stop listening";
+            listening = true;
+        };
+
+        const onResult = event => {
+            result.innerHTML = "";
+            for (const res of event.results) {
+                const text = document.createTextNode(res[0].transcript);
+                const p = document.createElement("p");
+                if (res.isFinal) {
+                    p.classList.add("final");
+                }
+                p.appendChild(text);
+                result.appendChild(p);
+
+                if (res[0].transcript.includes("stop")) {
+                    stop()
+                }
+            }
+        };
+        recognition.continuous = true;
+        recognition.interimResults = true;
+        recognition.addEventListener("result", onResult);
+        button.addEventListener("click", event => {
+            listening ? stop() : start();
+        });
+    } else {
+        button.remove();
+        const message = document.getElementById("message");
+        message.removeAttribute("hidden");
+        message.setAttribute("aria-hidden", "false");
+    }
+});
---
+++ speech.js
@@ -0,0 +1,65 @@
+const form = document.getElementById('voice-form');
+const input = document.getElementById('speech');
+const _main = document.getElementsByTagName('main')[1];
+const voiceSelect = document.getElementById('voices');
+let voices;
+let currentVoice;
+
+const populateVoices = () => {
+    const availableVoices = speechSynthesis.getVoices();
+    voiceSelect.innerHTML = '';
+
+    availableVoices.forEach(voice => {
+        const option = document.createElement('option');
+        let optionText = `${voice.name} (${voice.lang})`;
+        if (voice.default) {
+            optionText += ' [default]';
+            if (typeof currentVoice === 'undefined') {
+                currentVoice = voice;
+                option.selected = true;
+            }
+        }
+        if (currentVoice === voice) {
+            option.selected = true;
+        }
+        option.textContent = optionText;
+        voiceSelect.appendChild(option);
+    });
+    voices = availableVoices;
+};
+
+populateVoices();
+if (speechSynthesis.onvoiceschanged !== undefined) {
+    speechSynthesis.onvoiceschanged = populateVoices;
+}
+
+voiceSelect.addEventListener('change', event => {
+    const selectedIndex = event.target.selectedIndex;
+    currentVoice = voices[selectedIndex];
+});
+
+function speak(toSay) {
+    const utterance = new SpeechSynthesisUtterance(toSay);
+    utterance.voice = currentVoice;
+    utterance.addEventListener('start', event => {
+        _main.classList.add('speaking');
+    });
+    utterance.addEventListener('end', event => {
+        _main.addEventListener(
+            'animationiteration',
+            event => {
+                _main.classList.remove('speaking');
+            },
+            {
+                once: true
+            }
+        );
+    });
+    speechSynthesis.speak(utterance);
+    input.value = '';
+}
+
+form.addEventListener('submit', event => {
+    event.preventDefault();
+    speak("Hello World")
+});
